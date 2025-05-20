e886700b11fd55974bff37ed5c72d7d416d4821d
Jane-972 <wafaeboumajjane@gmail.com>
2025-05-20 02:46:26 +0200

    fetching from server succeeded

--- constants/Icons.js
+++ constants/Icons.js
@@ -1,10 +1,10 @@
 export const Icons = {
-    account : require('../assets/icons/account.png'),
-    chat : require('../assets/icons/chat.png'),
-    courses : require('../assets/icons/courses.png'),
-    home : require('../assets/icons/home.png'),
-    professors : require('../assets/icons/professors.png'),
-    srch : require('../assets/icons/search.png'),
-    bookmark : require('../assets/icons/bookmark.png'),
-    placeHolder : require('../assets/icons/placeHolder.png')
-}
+  account: require("../assets/icons/Frame8.png"),
+  chat: require("../assets/icons/Frame7.png"),
+  courses: require("../assets/icons/Frame6.png"),
+  home: require("../assets/icons/Frame4.png"),
+  professors: require("../assets/icons/Frame5.png"),
+  srch: require("../assets/icons/search.png"),
+  bookmark: require("../assets/icons/bookmark.png"),
+  placeHolder: require("../assets/icons/placeHolder.png"),
+};
